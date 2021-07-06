9dd6e8e3ffbd234f235697cb2ba93ec4e7da5f00
Samuel Cabral Cruz <samuelcabralcruz@gmail.com>
2021-07-05 22:17:03 -0400

    push vs sync

---
+++ sync/entrypoint.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+
+# check if pull_request event
+if [[ $GITHUB_EVENT_NAME != 'pull_request' ]]; then
+  echo "This action is designed to be run in pull request context. Quitting."
+  exit 1
+fi
+
+BUCKET_NAME=$1
+PROJECT_NAME=$2
+BYPASS_LABEL=$3
+
+# debug
+# cat $GITHUB_EVENT_PATH
+BASE_BRANCH_NAME=$(cat $GITHUB_EVENT_PATH | jq -r '.pull_request.base.ref')
+BRANCH_NAME=$(cat $GITHUB_EVENT_PATH | jq -r '.pull_request.head.ref')
+EVENT_TYPE=$(cat $GITHUB_EVENT_PATH | jq -r '.action')
+HAS_BYPASS_LABEL=$(cat $GITHUB_EVENT_PATH | jq ".pull_request | any(.labels[]; .name == \"$BYPASS_LABEL\")")
+IS_MERGED=$(cat $GITHUB_EVENT_PATH | jq -r '.pull_request.merged')
+
+# check if github token is set
+if [[ -z "$GITHUB_TOKEN" ]]; then
+  echo "GITHUB_TOKEN env variable is not set. Quitting."
+  exit 1
+fi
+
+# check if aws credentials are set
+if [[ -z "$AWS_ACCESS_KEY_ID" ]]; then
+  echo "AWS_ACCESS_KEY_ID env variable is not set. Quitting."
+  exit 1
+fi
+if [[ -z "$AWS_SECRET_ACCESS_KEY" ]]; then
+  echo "AWS_SECRET_ACCESS_KEY env variable is not set. Quitting."
+  exit 1
+fi
+if [[ -z "$AWS_REGION" ]]; then
+  AWS_REGION="us-east-1"
+fi
+# create a dedicated aws profile to avoid conflicts with other actions
+aws configure --profile free-code-coverage <<-EOF > /dev/null 2>&1
+${AWS_ACCESS_KEY_ID}
+${AWS_SECRET_ACCESS_KEY}
+${AWS_REGION}
+text
+EOF
+
+# check if bucket-name is provided and non-empty
+if [[ -z "$BUCKET_NAME" ]]; then
+  echo "bucket-name input is required. Quitting."
+  exit 1
+fi
+# check if bucket exists and reachable (AWS) (aws s3 ls $BUCKET_NAME)
+aws s3 ls $BUCKET_NAME --profile free-code-coverage &> /dev/null
+if [[ $? -ne 0 ]]; then
+  echo "Unreachable S3 bucket. Quitting."
+  exit 1
+fi
+# check if project-name is provided and non-empty and lower kebab case (regex: [a-z\-]+)
+if [[ -z "$PROJECT_NAME" ]]; then
+  echo "project-name input is required. Quitting."
+  exit 1
+fi
+if [[ ! "$PROJECT_NAME" =~ ^[a-z\-]+$ ]]; then
+  echo "project-name input is invalid. It should be a lower case kebab string (regex: [a-z\-]+). Quitting."
+  exit 1
+fi
+
+# if event_type is closed
+if [[ $EVENT_TYPE == 'closed' ]]; then
+  if [[ $IS_MERGED == 'true' ]]; then
+    # if pull_request is merged
+    echo 'Pull request closed and merged. Overriding base branch data.'
+    # rename coverage-metric file with base branch name
+    COVERAGE_METRIC_FILE_NAME="coverage-metric-$PROJECT_NAME-$BRANCH_NAME.txt"
+    COVERAGE_METRIC_S3_URI="s3://$BUCKET_NAME/$COVERAGE_METRIC_FILE_NAME"
+    BASE_COVERAGE_METRIC_FILE_NAME="coverage-metric-$PROJECT_NAME-$BASE_BRANCH_NAME.txt"
+    BASE_COVERAGE_METRIC_S3_URI="s3://$BUCKET_NAME/$BASE_COVERAGE_METRIC_FILE_NAME"
+    aws s3 mv $COVERAGE_METRIC_S3_URI $BASE_COVERAGE_METRIC_S3_URI --profile free-code-coverage
+    # rename badge file with base branch name
+    BADGE_FILE_NAME="badge-$PROJECT_NAME-$BRANCH_NAME.svg"
+    BADGE_S3_URI="s3://$BUCKET_NAME/$BADGE_FILE_NAME"
+    BASE_BADGE_FILE_NAME="badge-$PROJECT_NAME-$BASE_BRANCH_NAME.txt"
+    BASE_BADGE_S3_URI="s3://$BUCKET_NAME/$BASE_BADGE_FILE_NAME"
+    aws s3 mv $BADGE_S3_URI $BASE_BADGE_S3_URI --profile free-code-coverage
+  else
+    # if pull_request is not merged
+    echo 'Pull request closed, but not merged. Cleaning related data.'
+    # delete pull request coverage-metric file
+    COVERAGE_METRIC_FILE_NAME="coverage-metric-$PROJECT_NAME-$BRANCH_NAME.txt"
+    COVERAGE_METRIC_S3_URI="s3://$BUCKET_NAME/$COVERAGE_METRIC_FILE_NAME"
+    aws s3 rm $COVERAGE_METRIC_S3_URI --profile free-code-coverage
+    # delete pull request badge file
+    BADGE_FILE_NAME="badge-$PROJECT_NAME-$BRANCH_NAME.svg"
+    BADGE_S3_URI="s3://$BUCKET_NAME/$BADGE_FILE_NAME"
+    aws s3 rm $BADGE_S3_URI --profile free-code-coverage
+  fi
+  # delete coverage report comment url file
+  COVERAGE_REPORT_COMMENT_URL_FILE_NAME="coverage-report-comment-url-$PROJECT_NAME-$BRANCH_NAME.txt"
+  COVERAGE_REPORT_COMMENT_URL_S3_URI="s3://$BUCKET_NAME/$COVERAGE_REPORT_COMMENT_URL_FILE_NAME"
+  aws s3 rm $COVERAGE_REPORT_COMMENT_URL_S3_URI --profile free-code-coverage
+# if event_type is labeled or unlabeled
+elif [[ $EVENT_TYPE =~ ^(labeled|unlabeled)$ ]]; then
+  echo 'label update'
+  # TODO: manage status check here
+else
+  echo 'This action is designed to be run with pull_request event types: unlabeled, labeled, and closed. Quitting.'
+  exit 1
+fi
+
+# clear AWS credentials
+aws configure --profile free-code-coverage <<-EOF > /dev/null 2>&1
+null
+null
+null
+text
+EOF
